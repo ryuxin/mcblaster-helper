54f7c93f5241de0d0d9746116a6a619712dee557
graceliu <graceliu@node-1.eos-test.gwcloudlab-pg0.wisc.cloudlab.us>
2018-09-23 16:34:40 -0500

    server: fix server start and bind server to cpu

--- start-servers.py
+++ start-servers.py
@@ -1,6 +1,10 @@
 import subprocess
 import argparse
 
+nservers = 0
+cur_port = 0
+ncores = 0
+cur_core = 0
 
 class memcached(object):
     """
@@ -9,28 +13,48 @@ class memcached(object):
     Exposes a UDP and/or TCP port with the given port number(s).
     """
 
-    def __init__(self, tcp_port, udp_port, nb_threads, debug):
+    def __init__(self, tcp_port, udp_port, nb_threads, core, debug):
         self.tcp_port = tcp_port
         self.udp_port = udp_port
         self.nb_threads = nb_threads
         self.debug = debug
 
-        self.args = ["memcached"]
+        self.args = ["taskset"]
+        self.args.extend(["-c", str(core)])
+        self.args.extend(["memcached"])
         if debug:
             self.args.append("-vv")
-        self.args.extend(["-p", str(tcp_port), "-u", str(udp_port), "-t", str(nb_threads)])
+        if tcp_port > 0:
+            self.args.extend(["-p", str(tcp_port)])
+        self.args.extend([ "-U", str(udp_port), "-t", str(nb_threads)])
+        # print self.args
         self.p = subprocess.Popen(self.args)
 
     def stop(self):
         self.p.terminate()
 
 
+def increment_udp_port(port):
+    global cur_port
+    p = port + cur_port
+    cur_port = (cur_port +1) % nservers
+    return p
+
+
+def increment_core_num(core):
+    global cur_core
+    p = core + cur_core
+    cur_core = (cur_core +1) % ncores
+    return p
+
+
 def start_servers(tcp_port, udp_port, nb_threads, nb_servers, debug):
     mc_list = []
+
     for _ in range(nb_servers):
-        mc_list.append(memcached(tcp_port, udp_port, nb_threads, debug))
-        if udp_port > 0:
-            udp_port += 1
+        up = increment_udp_port(udp_port)
+        nc = increment_core_num(0)
+        mc_list.append(memcached(tcp_port, up, nb_threads, nc, debug))
         if tcp_port > 0:
             tcp_port += 1
     return mc_list
@@ -42,17 +66,20 @@ def parse_args():
     parser.add_argument("-u", help="udp port number", type=int)
     parser.add_argument("-t", help="Number of threads per server", type=int)
     parser.add_argument("--nb_servers", help="number of instances to create", type=int)
+    parser.add_argument("--nb_core", help="number of cores", type=int)
     parser.add_argument("--debug", help="Enable debugging", action="store_true")
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
-    tcp_port = args.p if args.p else 12345
+    tcp_port = args.p
     udp_port = args.u if args.u else 12345
     nb_threads = args.t if args.t else 4
     nb_servers = args.nb_servers if args.nb_servers else 1
     debug = args.debug
+    nservers = nb_servers
+    ncores = args.nb_core if args.nb_servers else 1
 
     mc_list = start_servers(tcp_port, udp_port, nb_threads, nb_servers, debug)
     while True:
