b0b403101a6816de07ef66f9397cf97a43b5f8cf
graceliu <graceliu@node-2.graceliu-qv41130.gwcloudlab-pg0.wisc.cloudlab.us>
2018-09-26 13:40:10 -0500

    mutli-test: update

--- multi_test.sh
+++ multi_test.sh
@@ -6,10 +6,18 @@ python start-clients.py \
 -k 100 \
 -t 1 \
 -z 135 \
--r 304000 \
--w 16000 \
+-r 6650 \
+-w 350 \
 --nb_clients $2 \
 --nb_servers $1 \
 --nb_core 32 \
---duration 10 \
+--duration 100 \
 --server 10.10.2.2
+
+#
+#-r 304000 \
+x#-w 16000 \
+
+# 10M
+# -r 6650 \
+# -w 350 \
